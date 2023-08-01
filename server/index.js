4bf41ecc1b7f4fbd06678b86e68d7ecddd778d4b
Haseeb Irfan <haseeb-sheikh10@192.168.10.13>
2023-08-01 19:04:27 +0500

    Client app build connected to backend

--- server/index.js
+++ server/index.js
@@ -2,13 +2,33 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-
 const app = express()
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 // midleware to use API Endpoints
 app.use(express.json());
 app.use(cors());
+
+
+// Serving client react app build 
+const path = require('path');
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname , "../client/build");
+
+app.use(express.static(buildPath));
+
+app.get('/*', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    (err) => {
+      if(err)
+      {
+        res.status(500).send(err);
+      }
+    }
+  )
+})
+
 
 //Available Routes
 app.use('/api/auth', require('./Routes/authRoute'));
